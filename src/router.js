b80b78ea9b1e67e1915d678a1d8c095b5034d255
Juan Pablo <juanpablojimenezc@outlook.com>
2025-08-23 16:24:45 -0500

    refactor: separate home view, update router and footer assets

--- src/router.js
+++ src/router.js
@@ -1,12 +1,21 @@
 const routes = {
-  "/": "../index.html",       // Landing page
-  "/recipes": "./views/recipes.html",
-  "/signin": "./views/signin.html",
-  "/signup": "./views/signup.html",
-  "/profile": "./pages/profile",
+  "/": "./src/pages/home/index.html",
+  "/recipes": "./src/pages/recipes/index.html",
+  "/signin": "./src/pages/login/index.html",
+  "/signup": "./src/pages/sign_up/index.html",
 };
 
+async function navigate(pathname) {
+  const route = routes[pathname];
+  if (!route) return;
 
+  const html = await fetch(route).then(res => res.text());
+  document.getElementById("content").innerHTML = html;
+
+  history.pushState({}, "", pathname);
+}
+
+// Soporte para clicks en links con data-link
 document.body.addEventListener("click", (e) => {
   if (e.target.matches("[data-link]")) {
     e.preventDefault();
@@ -15,10 +24,5 @@ document.body.addEventListener("click", (e) => {
   }
 });
 
-async function navigate(pathname) {
-  const route = routes[pathname];
-  const html = await fetch(route).then((res) => res.text());
-  document.getElementById("content").innerHTML = html;
-  history.pushState({}, "", pathname);
-}
-
+// Cargar home al inicio
+navigate("/");
